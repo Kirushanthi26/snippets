01f674c3610e552537c8b88a9be759d39757d16c
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-09 15:30:11 +0530

    server action for snippet

---
+++ features/snippets/services/snippet.actions.ts
@@ -0,0 +1,23 @@
+"use server"; //this needs to be a server action
+
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+
+export async function createSnippet(formData: FormData) {
+  // check the user's input and make sure they're valid
+  const title = formData.get("title") as string;
+  const code = formData.get("code") as string;
+
+  //create a new record in the database
+  const snippet = await prisma.snippet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+
+  console.log(snippet);
+
+  //redirect the user back to the root route
+  redirect("/");
+}
