f25b9f4f9a026bd3b6a4c4170eb85e6a53e805f0
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-17 15:34:45 +0530

    Not Found file

---
+++ app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFound() {
+    return (
+        <div>
+            <h1 className="text-center text-3xl font-semibold">Sorry, we could not find this Snippet!!!</h1>
+        </div>
+    )
+}
