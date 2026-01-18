873e2e37a2c1c44076f235cfbf7bf4220db15b13
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-18 20:03:44 +0530

    style add

--- features/snippets/components/Snippet.tsx
+++ features/snippets/components/Snippet.tsx
@@ -7,8 +7,18 @@ type SnippetProps = {
 export default function Snippet({ snippet }: SnippetProps) {
     return (
         <div>
-            <h1>{snippet.title}</h1>
-            <pre>{snippet.code}</pre>
+            <div className="flex m-4 justify-between items-center">
+                <h1 className="text-2xl font-bold capitalize">{snippet.title}</h1>
+                <div className="space-x-3">
+                    <button className="p-2 border rounded capitalize">Edit</button>
+                    <button className="p-2 border rounded capitalize">delete</button>
+                </div>
+            </div>
+            <pre className="p-3 border rounded bg-gray-200 border-gray-300">
+                <code>
+                    {snippet.code}
+                </code>
+            </pre>
         </div>
     );
 }
