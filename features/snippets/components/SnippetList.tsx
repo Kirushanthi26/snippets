5355fe45920d9b8f05caaefb5a42e165e06efe00
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-18 19:54:13 +0530

    add nav between form and snippet based on id

--- features/snippets/components/SnippetList.tsx
+++ features/snippets/components/SnippetList.tsx
@@ -1,13 +1,20 @@
 import { snippet } from "@/types"
+import Link from "next/link"
 
 type SnippetListProps = {
     snippets: snippet[]
 }
 
 export default function SnippetList({ snippets }: SnippetListProps) {
-    return <div>
-        {snippets.map((snippet) => <div key={snippet.id}>
+    return <div className="flex flex-col gap-5 mt-5">
+        <div className="flex flex-row justify-between items-center">
+            <h3 className="text-2xl font-semibold ">snippets</h3>
+            <Link href={"/snippets/new"} className="border p-2 rounded">New</Link>
+        </div>
+
+        {snippets.map((snippet) => <Link href={`/snippets/${snippet.id}`} key={snippet.id} className="flex justify-between items-center p-2 border rounded">
             <h1>{snippet.title}</h1>
-        </div>)}
+            <div>view</div>
+        </Link>)}
     </div>
 }
