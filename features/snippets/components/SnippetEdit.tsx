14342bde123e973b95fad720bffc9e350cefb371
Kirushanthi26 <kirushanthikiru26@gmail.com>
2026-01-19 10:28:00 +0530

    implement monaco code editor

--- features/snippets/components/SnippetEdit.tsx
+++ features/snippets/components/SnippetEdit.tsx
@@ -1,11 +1,30 @@
 'use client'
 
 import { snippet } from "@/types"
+import Editor from '@monaco-editor/react';
+import { useState } from "react";
 
 type SnippetEditProps = {
     snippet: snippet
 }
 
 export default function SnippetEdit({ snippet }: SnippetEditProps) {
-    return <div>hello {snippet.title}</div>
+    const [code, setCode] = useState<string>(snippet.code)
+
+    function handleEditorChange(value: string = "") {
+        setCode(value)
+    }
+
+
+    return <div className="space-y-5">
+        <h1 className="text-2xl font-semibold">Edit {snippet.title}</h1>
+        <Editor
+            height="40vh"
+            defaultLanguage="javascript"
+            theme="vs-dark"
+            onChange={handleEditorChange}
+            defaultValue={code}
+        />
+        <button type="submit" className="rounded p-2 bg-blue-200 ">Save</button>
+    </div>
 }
